9104c42adf68643600e1e435bc741e252d65d679
maadZia <mbernatagh@gmail.com>
2024-10-15 12:37:52 +0200

    dodana funkcja

--- main.py
+++ main.py
@@ -16,6 +16,19 @@ def load_text_files(folder_path):
     return text
 
 
+# do sprawdzenia wynikow dla poszczegolnych tekstow
+def load_simple_file(folder_path):
+    file_name = "text1.txt" # mozna zmieniac
+    file_path = os.path.join(folder_path, file_name)
+
+    if os.path.isfile(file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text = file.read()
+        return text
+    else:
+        return "File not found."
+
+
 def clean(text):
     text = re.sub(r'[^a-zA-ZĂăÂâÎîȘșȚț\s-]', '', text)
     text = text.lower()
@@ -24,10 +37,10 @@ def clean(text):
     filtered_words = [word for word in words if word not in word_to_remove]
     return filtered_words
 
+
 def zipf(words):
     word_counter = Counter(words)
     frequency = word_counter.most_common()
-
 
     with open("wynik.csv", 'w', newline='', encoding='utf-8') as csvfile:
         csv_writer = csv.writer(csvfile)
@@ -35,6 +48,7 @@ def zipf(words):
 
         for rang, (word, value) in enumerate(frequency, start=1):
             csv_writer.writerow([word, value, rang, value * rang])
+
 
 def select_percentage(words,percent):
     word_counter = Counter(words)
@@ -62,13 +76,14 @@ def word_graph(words, limit=100):
         G.add_edge(limited_words[i], limited_words[i+1])
 
     plt.figure(figsize=(12, 12))
-    pos = nx.spring_layout(G, k=0.8)  #wieksze k = wieksza odleglosc miedzy wierzcholkami
+    pos = nx.spring_layout(G, k=0.7)  # wieksze k = wieksza odleglosc miedzy wierzcholkami
     nx.draw(G, pos, with_labels=True, node_size=1000, node_color="skyblue", font_size=6, font_weight="bold")
 
     plt.savefig('word_graph.png', format="png")
     plt.close()
 
 
+# whole_text = load_simple_file("Files")
 whole_text = load_text_files("Files")
 words = clean(whole_text)
 zipf(words)
